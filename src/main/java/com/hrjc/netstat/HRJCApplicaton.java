9c3455216733176d30e822eea40c2de7c9970539
yintao <279253462@qq.com>
2022-03-19 21:45:45 +0800

    HRJC v1.0

---
+++ src/main/java/com/hrjc/netstat/HRJCApplicaton.java
@@ -0,0 +1,13 @@
+package com.hrjc.netstat;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class HRJCApplicaton {
+
+    public static void main(String[] args)
+    {
+         SpringApplication.run(HRJCApplicaton.class,args);
+    }
+}
